e35057e5e8b5dc924643d8e08ab4023b21500f59
Sathwik Annaldesh <93048377+Sathwik041@users.noreply.github.com>
2026-02-01 23:35:58 +0530

    Create SAT_TOKEN_NFT contract with minting functionality

---
+++ SAT_TOKEN_NFT.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.5.0
+pragma solidity ^0.8.27;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SAT_TOKEN is ERC721, ERC721URIStorage, Ownable {
+    constructor(address initialOwner)
+        ERC721("SAT_TOKEN", "SAT")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
